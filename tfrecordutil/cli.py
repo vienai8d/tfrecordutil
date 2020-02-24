4f40681b328db5f24966fdc258e60509a251e59a
vienai8d <31370233+vienai8d@users.noreply.github.com>
2020-02-25 00:42:02 +0900

    fillna for tf.string columns

--- tfrecordutil/cli.py
+++ tfrecordutil/cli.py
@@ -1,6 +1,7 @@
 import os
 import tensorflow as tf
 import pandas as pd
+import numpy as np
 
 from argparse import ArgumentParser
 from tfrecordutil import create_example_schema, write_example_tfrecord
@@ -16,10 +17,17 @@ def csv2tfrecord(arguments=None):
     parser = ArgumentParser()
     parser.add_argument('input', help='CSV file')
     parser.add_argument('output', help='TFRrecord file')
-    args = parser.parse_args()
+    args = parser.parse_args(arguments)
 
     try:
         df = pd.read_csv(args.input)
+
+        for k, v in df.dtypes.items():
+            if v != np.object_:
+                continue
+            if df[k].isnull().sum() > 0:
+                df[k] = df[k].fillna('')
+
         schema = create_example_schema(df)
         dataset = tf.data.TFRecordDataset(args.input)
         write_example_tfrecord(args.output, dataset, schema)
